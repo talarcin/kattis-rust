420644998ae9536a36e73cb3f808a03d66ad7c4f
talarcin <tuncayalarcin@gmail.com>
2023-12-25 18:05:56 +0100

    Refactor solution fucntions

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -1,14 +1,68 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::{HashMap, HashSet};
+
+pub fn make_graph(pairs: &Vec<(usize, usize)>) -> Graph {
+    let mut graph: Graph = Graph::new();
+    for pair in pairs {
+        graph.add_node(&pair.0);
+        graph.add_node(&pair.1);
+        graph.connect_nodes(&pair.0, &pair.1);
+    }
+
+    graph
+}
+
+pub fn make_complementary_graph(graph: &Graph) -> Graph {
+    let mut comp_graph: Graph = Graph::new();
+
+    for (u, edges) in &graph.nodes {
+        comp_graph.add_node(&u);
+
+        for v in &graph.get_nodes() {
+            if !edges.contains(v) {
+                comp_graph.add_node(v);
+                comp_graph.connect_nodes(u, v);
+            }
+        }
+    }
+
+    comp_graph
+}
+
+pub struct Graph {
+    nodes: HashMap<usize, HashSet<usize>>,
+}
+
+impl Graph {
+    fn new() -> Graph {
+        Graph {
+            nodes: HashMap::new(),
+        }
+    }
+
+    fn add_node(self: &mut Self, u: &usize) {
+        if !self.nodes.contains_key(u) {
+            self.nodes.insert(*u, HashSet::new());
+        }
+    }
+
+    fn connect_nodes(self: &mut Self, u: &usize, v: &usize) {
+        self.nodes.get_mut(u).unwrap().insert(*v);
+        self.nodes.get_mut(v).unwrap().insert(*u);
+    }
+
+    fn get_nodes(self: &Self) -> Vec<usize> {
+        let mut node_vec = Vec::new();
+        for (u, _) in &self.nodes {
+            node_vec.push(*u);
+        }
+
+        node_vec
+    }
+
+    fn size(&self) -> usize {
+        self.nodes.values().count()
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
--- icy_itinerary/src/lib.rs
+++ icy_itinerary/src/lib.rs
@@ -1,27 +1,37 @@
-use std::collections::HashSet;
+use graph::{make_complementary_graph, make_graph, Graph};
 
-pub struct Node {
-    value: usize,
-    adjacent: HashSet<Node>,
+pub fn solve() -> String {
+    String::from("not implemented")
 }
 
-pub struct Graph {
-    nodes: HashSet<Node>,
+pub fn algorithm(lines: Vec<String>) {
+    let (n, m, pairs) = parse_input(lines);
+    let mut graph: Graph = make_graph(&pairs);
+    let mut comp_graph: Graph = make_complementary_graph(&graph);
 }
 
-impl Node {
-    fn value(&self) -> &usize {
-        &self.value
+pub fn parse_input(lines: Vec<String>) -> (usize, usize, Vec<(usize, usize)>) {
+    let desc: Vec<usize> = lines[0]
+        .split_whitespace()
+        .map(|v| v.parse().unwrap())
+        .collect();
+
+    let n = desc[0];
+    let m = desc[1];
+
+    let mut pairs: Vec<(usize, usize)> = Vec::new();
+
+    for i in 1..=m {
+        let pair: Vec<usize> = lines[i]
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        pairs.push((pair[0], pair[1]));
     }
 
-    fn adj(&self) -> &HashSet<Node> {
-        &self.adjacent
-    }
+    (n, m, pairs)
 }
-
-impl Graph {}
-
-pub fn solve() -> String {}
 
 #[cfg(test)]
 mod tests {}
