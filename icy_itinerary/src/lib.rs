7bcc6386ea1a6824eeaceed51231ac997b141233
t.alarcin <tuncayalarcin@gmail.com>
2024-01-10 15:54:00 +0100

    Change comments to en

--- icy_itinerary/src/lib.rs
+++ icy_itinerary/src/lib.rs
@@ -2,121 +2,125 @@ use std::collections::{HashMap, HashSet, LinkedList};
 
 pub fn solve(lines: Vec<String>) -> String {
     let (n, _, adj) = parse_input(lines);
-
-    // Intialisiere Listen für Schneeweg und Straßenweg und String für Startliste
     let mut start = "".to_string();
     let mut snow_list: LinkedList<usize> = LinkedList::new();
     let mut road_list: LinkedList<usize> = LinkedList::new();
-
     let mut path: String = String::from("");
 
-    // Ist Knoten 1 mit 2 verbunden, existiert Straße zwischen 1 und 2, also starten wir auf der Straße
-    // und fügen 1 und 2 ans Ende der Straßenliste
+    // Is node 1 connected with 2, i.e. there is a street between them,
+    // then we start on a road path and push 1 and 2 to the end of road_list
+    // Other wise we start on a snow path and push
+    // 1 and 2 to the end of snow_list
     if adj.get(&1).unwrap().contains(&2) {
         start = String::from("r");
         road_list.push_back(1);
         road_list.push_back(2);
-    }
-    // Ist Knoten 1 mit 2 nicht verbunden, existiert keine Straße zwischen 1 und 2, also starten wir auf dem Schneeweg
-    // und fügen 1 und 2 ans Ende der Schneeliste
-    else {
+    } else {
         start = String::from("s");
         snow_list.push_back(1);
         snow_list.push_back(2);
     }
 
-    // Iteriere über alle Knoten von 3 bis n
+    // Iterate over all nodes 3 to n
     for z in 3..=n {
-        // Wir haben die Situation road-snow
+        // Here we have the situation road-snow
         if start == String::from("r") {
-            //  x ist der letzte Knoten in der Straßenliste und y der erste Knoten in der Schneeliste
+            // x is the last node of the road list and
+            // y the first node of the snow list
             let x = road_list.back().unwrap();
             let y = snow_list.front();
 
-            // Wenn x mit z verbunden ist und y nicht mit z verbunden ist oder nicht existiert, füge z ans Ende der Straßenliste
+            // If x is connected with z and
+            // y not we push z to the end of road_list
             if adj.get(x).unwrap().contains(&z)
                 && (y == None || !adj.get(y.unwrap()).unwrap().contains(&z))
             {
                 road_list.push_back(z);
             }
-            // Wenn x und y mit z verbunden sind oder y nicht existiert, füge z an das Ende der Straßenliste und y auch
+            // If x and y are connected with z
+            // we put z at the end of road_list and y also
             else if adj.get(x).unwrap().contains(&z)
                 && (y == None || adj.get(y.unwrap()).unwrap().contains(&z))
             {
                 road_list.push_back(z);
                 road_list.push_back(snow_list.pop_front().unwrap());
             }
-            // Wenn x nicht mit z verbunden ist und y nicht mit z verbunden oder nicht existiert ist oder nicht existiert, füge z an den Anfang der Schneeliste
+            // If x is neither connected with z nor with y
+            // we push z to the beginning of snow_list
             else if !adj.get(x).unwrap().contains(&z)
                 && (y == None || !adj.get(y.unwrap()).unwrap().contains(&z))
             {
                 snow_list.push_front(z);
             }
-            // Wenn x nicht mit z verbunden ist und y mit z verbunden ist oder nicht existiert, dann
-            // füge letzten Knoten von Straßenliste an Ende der Schneeliste und
-            // betrachte Vorgänger von x
+            // If x is not connected with z but y is
+            // we pop x from road_list and
+            // push it to the beginning of snow_list
+            // and look at the precursor node of x
             else if !adj.get(x).unwrap().contains(&z)
                 && (y == None || adj.get(y.unwrap()).unwrap().contains(&z))
             {
                 snow_list.push_front(road_list.pop_back().unwrap());
                 let pre_x = road_list.back();
 
-                // Wenn pre_x mit z verbunden ist, füge z an das Ende der Straßenliste,
-                // ansonsten füge z an den Anfang der Schneeliste
+                // If pre_x is connected with z we push z to
+                // the end of road_list, otherwise we push it
+                // to the front of snow_list
                 if adj.get(pre_x.unwrap()).unwrap().contains(&z) {
                     road_list.push_back(z);
                 } else {
                     snow_list.push_front(z);
                 }
 
-                // Ist nun letzter Knoten in Straßenliste 1, so füge 1 an Schneeliste vorne an und
-                // setze Start auf Schneeweg
+                // In case the remaining node of road_list is 1
+                // we switch the situation to snow-road
                 if road_list.back().unwrap() == &1 {
                     snow_list.push_front(road_list.pop_back().unwrap());
                     start = String::from("s");
                 }
             }
         }
-        // Wir haben die Situation snow-road
+        // The situation is snow-road
         else {
-            // x ist der erste Knoten in der Straßenliste und y der letzte Knoten in der Schneeliste
+            // x ist the first node of snow_list and y the
+            // last node of road_list
             let x = road_list.front();
             let y = snow_list.back().unwrap();
 
-            // Wenn x mit z verbunden ist oder nicht existiert und y nicht mit z verbunden ist, füge z an den Anfang der Straßenliste
+            // If x is connected with z and y not, we push z to the start of road_list
             if (x == None || adj.get(x.unwrap()).unwrap().contains(&z))
                 && !adj.get(y).unwrap().contains(&z)
             {
                 road_list.push_front(z);
             }
-            // Wenn x mit z verbunden oder nicht existiert ist und y mit z verbunden ist, füge erst z an den Anfang der Straßenliste und dann y
-            // Ist dann die Schneeliste leer, setze Start auf Straßenweg
+            // If x is connect with z and y also,
+            // we push z to start of road_list and then y
             else if (x == None || adj.get(x.unwrap()).unwrap().contains(&z))
                 && adj.get(y).unwrap().contains(&z)
             {
                 road_list.push_front(z);
                 road_list.push_front(snow_list.pop_back().unwrap());
+
+                // If the snow list is empty, we switch to situation
                 if snow_list.len() == 0 {
                     start = String::from("r");
                 }
             }
-            // Wenn x nicht mit z verbunden ist oder nicht existiert und y nicht mit z verbunden ist, füge z an das Ende der Schneeliste
+            // If x and y are not connected with z, we push z to the end of snow_list
             else if (x == None || !adj.get(x.unwrap()).unwrap().contains(&z))
                 && !adj.get(y).unwrap().contains(&z)
             {
                 snow_list.push_back(z);
             }
-            // Wenn x nicht mit z verbunden ist oder nicht existiert und y mit z verbunden ist,
-            // füge x ersten Knoten der Straßenliste an Ende der Schneeliste und
-            // betrachte Nachfolger von x
+            // If x ist not connected with z but y is,
+            // we push x to end of snow_list and then look at node after x
             else if (x == None || !adj.get(x.unwrap()).unwrap().contains(&z))
                 && adj.get(y).unwrap().contains(&z)
             {
                 snow_list.push_back(road_list.pop_front().unwrap());
                 let post_x = road_list.front();
 
-                // Ist post_x mit z verbunden, füge z an den Anfang der Straßenliste,
-                // ansonsten füge z ans Ende der Schneeliste
+                // If post_x is connected with z, we push z to start of road_list
+                // otherwise to the end of snow_list
                 if post_x == None || adj.get(post_x.unwrap()).unwrap().contains(&z) {
                     road_list.push_front(z);
                 } else {
@@ -126,7 +130,7 @@ pub fn solve(lines: Vec<String>) -> String {
         }
     }
 
-    // Erstelle abhängig von Start die Ausgabe
+    // Generate output path based on start situation
     if start == String::from("r") {
         path = make_path(&road_list, &snow_list);
     } else {
